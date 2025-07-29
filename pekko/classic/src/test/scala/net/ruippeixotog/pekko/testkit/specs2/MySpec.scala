1305a78ebf969ec992919056fa50ad62a924d581
Beatriz Magalhães <bmagalhaes@kevel.com>
2025-07-29 20:42:29 +0100

    Remove unnecessary casts

--- pekko/classic/src/test/scala/net/ruippeixotog/pekko/testkit/specs2/MySpec.scala
+++ pekko/classic/src/test/scala/net/ruippeixotog/pekko/testkit/specs2/MySpec.scala
@@ -13,36 +13,36 @@ class MySpec extends PekkoSpecification {
 
     "receive messages" in {
       testActor ! "hello" // expect any message
-      (this: TestKitBase) must receiveMessage
+      this must receiveMessage
 
       testActor ! "hello" // expect a specific message
-      (this: TestKitBase) must receive("hello")
+      this must receive("hello")
 
       testActor ! "hello" // expect a message of a given type
-      (this: TestKitBase) must receive[String]
+      this must receive[String]
 
       testActor ! "hello" // expect a message matching a function
-      (this: TestKitBase) must receive[String].which(_ must startWith("h"))
+      this must receive[String].which(_ must startWith("h"))
 
       testActor ! Some("hello") // expect a message matching a partial function
-      (this: TestKitBase) must receive.like { case Some(s: String) =>
+      this must receive.like { case Some(s: String) =>
         s must startWith("h")
       }
 
       testActor ! "b" // expect several messages, possibly unordered
       testActor ! "a"
-      (this: TestKitBase) must receive.allOf("a", "b")
+      this must receive.allOf("a", "b")
 
       testActor ! "b" // expect a message (possibly not the next one)
       testActor ! "a"
-      (this: TestKitBase) must receive("a").afterOthers
+      this must receive("a").afterOthers
 
       testActor ! "hello" // expect a message with an explicit timeout
-      (this: TestKitBase) must receiveWithin(1.second)("hello")
+      this must receiveWithin(1.second)("hello")
 
       case class Envelope(msg: String) // unwrap a message before matching
       testActor ! Envelope("hello")
-      (this: TestKitBase) must receive[Envelope].unwrap(_.msg).which(_ must startWith("h"))
+      this must receive[Envelope].unwrap(_.msg).which(_ must startWith("h"))
 
       testActor ! "hello" // ...and several combinations of the modifiers above
       (this: TestKitBase) must receiveWithin(1.second)[String].which(_ must startWith("h")).afterOthers
