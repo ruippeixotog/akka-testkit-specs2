eb8dd2512d808033442fd30e1d555e1ae2ae9ccf
Bruno Mendes <bd_mendes@outlook.com>
2026-01-11 01:32:45 +0000

    Fix generic failing line in Scala 3/specs2_5 (#311)
    * Make PekkoSpecification extend Specification
    
    * Make PekkoTypedSpecification extend Specification

--- pekko/classic/src/main/scala/net/ruippeixotog/pekko/testkit/specs2/mutable/PekkoSpecification.scala
+++ pekko/classic/src/main/scala/net/ruippeixotog/pekko/testkit/specs2/mutable/PekkoSpecification.scala
@@ -3,13 +3,17 @@ package net.ruippeixotog.pekko.testkit.specs2.mutable
 import java.util.UUID
 
 import org.apache.pekko.actor.ActorSystem
-import org.apache.pekko.testkit.TestKit
+import org.apache.pekko.testkit.TestKitBase
+import org.specs2.mutable.Specification
 
 import PekkoSpecification._
 
 abstract class PekkoSpecification(_system: ActorSystem = actorSystemForClass(getClass))
-    extends TestKit(_system)
-    with PekkoSpecificationLike
+    extends Specification
+    with TestKitBase
+    with PekkoSpecificationLike {
+  implicit def system: ActorSystem = _system
+}
 
 object PekkoSpecification {
   private def sanitizeName(name: String) = name.replaceAll("[^a-zA-Z0-9-]", "_")
--- pekko/typed/src/main/scala/net/ruippeixotog/pekko/testkit/specs2/mutable/PekkoTypedSpecification.scala
+++ pekko/typed/src/main/scala/net/ruippeixotog/pekko/testkit/specs2/mutable/PekkoTypedSpecification.scala
@@ -1,5 +1,8 @@
 package net.ruippeixotog.pekko.testkit.specs2.mutable
 
 import org.apache.pekko.actor.testkit.typed.scaladsl.ActorTestKit
+import org.specs2.mutable.Specification
 
-abstract class PekkoTypedSpecification(val testKit: ActorTestKit = ActorTestKit()) extends PekkoTypedSpecificationLike
+abstract class PekkoTypedSpecification(val testKit: ActorTestKit = ActorTestKit())
+    extends Specification
+    with PekkoTypedSpecificationLike
